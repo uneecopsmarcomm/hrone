ccdf3d6999ccddb609dfa3643e0f64d0002c3158
BeingPrashaant <er.prashaant@gmail.com>
2018-11-28 18:49:43 +0530

    Update

--- index.php
+++ index.php
@@ -29,10 +29,10 @@ get_header(); ?>
 <?php else : ?>
 <?php endif; // end have_posts() check ?>
 
-<section id="blog-side-menu">
+<section>
 	<div class="container">
 		<div class="row">
-			<div class="col s2 side-col">
+			<div class="col s2 blog-side-menu">
 				<h3 class="side-heading">Resource <span>Centre</span></h3>
 				<ul>
 					<li class="active"> 
@@ -65,8 +65,50 @@ get_header(); ?>
 			<div class="col s10">
 				<div class="blog-wrapper">
 					<div class="container">
-						<div class="col s12">
-							<input type="search" placeholder="Let's find what you're looking for…">
+						<div class="row">
+							<div class="col s12">
+								<div class="search-input">
+									<input type="search" placeholder="Let's find what you're looking for…">
+									<i class="material-icons">search</i>
+								</div>
+							</div>
+						</div>					
+						<div class="row">
+							<div class="col s12">
+								<ul class="tags">
+									<li class="tag active">Payroll</li> 
+									<li class="tag">Engagement</li>
+									<li class="tag">Peformance</li>
+									<li class="tag">Training</li>
+									<li class="tag">Compliance</li>
+									<li class="tag">HR-Tech</li>
+									<li class="tag">Time Management</li>
+									<li class="tag">Recruitment</li>
+								</ul>
+								<span class="next-circle"></span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col s8">
+								<div class="featured-post z-depth-1">
+									<a href="">
+										<img src="<?php echo get_template_directory_uri(); ?>/img/blog/featured-img-01.jpg" alt="">
+									</a>
+									<div class="featured-tag">FEATURED</div>
+									<div class="detail-card">
+										<div>
+											<span class="post-dated"><i class="dated-icon"></i>28 Nov 2018</span>
+											<span class="post-read pull-right"><i class="read-icon"></i>10 Min Read</span>
+										</div>
+										<p>Top Four Reasons to Switch to a Cloud Based HRMS</p>
+									</div>
+								</div>
+							</div>
+							<div class="col s4">
+								<div class="sub-box z-depth-1">
+
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
